81824eb6895b692f067d32337bc763346f31e8ec
Christopher Barden <christopherbarden@yahoo.com>
2017-11-29 17:03:43 -0500

    Committing smallest working bot

--- zamielBot.py
+++ zamielBot.py
@@ -8,10 +8,14 @@ async def on_ready():
     print(bot.user.id)
     print("----------")
 
+@bot.command()
+async def joined(member : discord.Member):
+    """Says when a member joined."""
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
 async def _bot():
     await bot.say("I'm here bitches")
 
 with open(".token", 'r') as tokenFile:
     token = tokenFile.read()
-    print(token)
-    bot.run(token)
+    bot.run(token.strip())
