3df44de80d713f1bde977a5264cc5e71cb0b5221
Christopher Barden <christopherbarden@yahoo.com>
2017-12-05 19:35:05 -0500

    First mongodb edit

--- zamielBot.py
+++ zamielBot.py
@@ -1,6 +1,15 @@
 import discord
+import mongoengine
 from discord.ext import commands
 
+class userMention(mongoengine.Document):
+    mentionFrom = discord.User(Required=True)
+    mentionTo = discord.User(Required=True)
+
+def __init__():
+    mongoengine.connect('discordMentions')
+
+mentions = {}
 bot = commands.Bot(command_prefix='&', description='''Zamiel's Place home bot''')
 async def on_ready():
     print("Logged in as")
@@ -15,7 +24,21 @@ async def joined(member : discord.Member):
 
 async def _bot():
     await bot.say("I'm here bitches")
+    
+@bot.event
+async def on_message(message):
+    if(len(message.mentions) < 1):
+        pass
+    else:
+        for sender in message.mentions:
+            if(message.author.id + "To" + sender.id in mentions):
+                mentions[message.author.id + "To" + sender.id] += 1
+            else:
+                mentions[message.author.id + "To" + sender.id] = 1
 
+    await bot.process_commands(message)
+
+__init__()
 with open(".token", 'r') as tokenFile:
     token = tokenFile.read()
     bot.run(token.strip())
